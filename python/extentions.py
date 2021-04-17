613b7d4be739ca1c1437dbbd358d9602dc6a2a88
Titov Evgeniy <titov32@gmail.com>
2021-04-17 14:25:13 +0700

    Add files via upload
    adding extentions.py file which parsing xlsx file

---
+++ python/extentions.py
@@ -0,0 +1,18 @@
+import openpyxl  # импортируем модуль дря работы с Экселем
+
+# Load book excel
+wb = openpyxl.load_workbook('ШУ ТЗ.xlsx')
+
+# get name sheet and load it
+sheet_name = wb.sheetnames[-1]
+sheet = wb[sheet_name]
+printers={}
+# pass for diapason in book and print
+for i in sheet['A2':'N74']:
+    if i[9]:
+        printers[i[9].value] = {
+            'Местоположение': f'{i[1].value}  {i[4].value}  {i[5].value}',
+            'IP': i[11].value,
+            'Инвентарный номер':i[7].value,
+            'РМ':i[13].value
+                                }
