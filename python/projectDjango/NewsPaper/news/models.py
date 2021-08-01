3d712df22e27d7355f8c0e231be00acb80ea7da2
Titov Evgeniy <titov32@gmail.com>
2021-08-01 20:49:29 +0700

    Added view and templates for show content, edited models for better viewing

--- python/projectDjango/NewsPaper/news/models.py
+++ python/projectDjango/NewsPaper/news/models.py
@@ -9,7 +9,7 @@ class Author(models.Model):
     ratingAuthor = models.SmallIntegerField(default=0)
 
     def __str__(self):
-        return f'self.user.username'
+        return f'{self.user.username}'
 
     # собираем все связанные посты с автором, применяем к нему метод aggregate и высчитваем сумму по полю 'rating'
     def update_rating(self):
@@ -33,7 +33,7 @@ class Category(models.Model):
     category = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
-        return f'self.category'
+        return f'{self.category}'
 
 
 class Post(models.Model):
@@ -75,7 +75,8 @@ class Post(models.Model):
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
+    def __str__(self):
+        return f' {self.post.title} : {self.category.category[:20]}'
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -85,7 +86,7 @@ class Comment(models.Model):
     rating = models.IntegerField(default=0)
 
     def __str__(self):
-        return f' self.user.username,  self.text[:20]'
+        return f' {self.user.username},  {self.text[:20]}'
 
     def like(self):
         self.rating += 1
--- python/projectDjango/NewsPaper/news/views.py
+++ python/projectDjango/NewsPaper/news/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostsList(ListView):
+    # указываем модель, объекты которой мы будем выводить
+    model = Post
+    # указываем имя шаблона, в котором будет лежать html, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    template_name = 'news/news.html'
+    # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через html-шаблон
+    context_object_name = 'posts'
+
+class PostDetail(DetailView):
+
+    # указываем модель, объекты которой мы будем выводить
+    model = Post
+    # указываем имя шаблона, в котором будет лежать html, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    template_name = 'news/new.html'
+    # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через html-шаблон
+    context_object_name = 'post'
