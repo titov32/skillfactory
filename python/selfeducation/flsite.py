2d530a473868e829954feac069318a74191e85db
Titov Evgeniy <titov32@gmail.com>
2021-09-02 13:21:15 +0700

    update route skud parsing

--- python/selfeducation/flsite.py
+++ python/selfeducation/flsite.py
@@ -1,38 +1,40 @@
 from flask import Flask, render_template, url_for, request
 from parsing import ParserSS
 from time import sleep
+
 app = Flask(__name__)
 
-
-@app.route('/parse', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/parse', methods=['GET', 'POST'])
 def parse():
-    if request.method == 'POST':
-        print(request.form)
     a = ParserSS()
     a.register()
+    if request.method == 'POST':
+        print(request.form)
 
-    url_parsing = request.form.get('url_parsing')
-    print(url_parsing, ' это переменная url parsing')
+        url_parsing = request.form.get('url_parsing')
+        print(url_parsing, ' это переменная url parsing')
 
-    if not url_parsing:
-        print('ветка1')
-        return render_template('permit_not_found.html')
-    try:
-        print('ветка2')
-        id_user = url_parsing.split('id=')[-1]
-    except Exception as e:
-        print('Ошибка ввода ', e)
+        if not url_parsing:
+            print('ветка1')
+            return render_template('permit_not_found.html')
+        try:
+            print('ветка2')
+            id_user = url_parsing.split('id=')[-1]
+        except Exception as e:
+            print('Ошибка ввода ', e)
 
+        e = a.parse_employ(id_user)
+        print('Фамилия', e['family'])
+        print('special', e['special'])
+        print('department', e['department'])
+        print('tabel', e['tabel'])
+        return render_template('permit.html', employ=e)
+    else:
+        r = a.parse_employ('9')
+        return render_template('permit.html', employ=r)
 
-    e = a.parse_employ(id_user)
-    print('Фамилия', e['family'])
-    print('special', e['special'])
-    print('department', e['department'])
-    print('tabel', e['tabel'])
-    return render_template('permit.html', employ=e)
-
-
-@app.route('/parse_unbound', methods=['GET','POST'])
+@app.route('/parse_unbound', methods=['GET', 'POST'])
 def parse_unbound():
     if request.method == 'POST':
         print(request.form)
@@ -50,7 +52,7 @@ def parse_unbound():
     return render_template('permits.html', employs=employs)
 
 
-@app.route('/parse_unbound_one_permit', methods=['GET','POST'])
+@app.route('/parse_unbound_one_permit', methods=['GET', 'POST'])
 def parse_unbound_one_permit():
     if request.method == 'POST':
         print(request.form)
