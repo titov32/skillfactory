2447232ffef69dccab7212e05e594477d8baad0c
Titov Evgeniy <titov32@gmail.com>
2021-09-13 14:25:08 +0700

    Move some projects to isolated repo

--- python/selfeducation/setting.py
+++
@@ -1,9 +0,0 @@
-# This file local settings
-import redis
-
-USER = {
-    'login': 'e.titov',
-    'domain': 'SHAHTA12',
-    'password': '47tit_evg'
-}
-CONN_REDIS = redis.Redis(host='localhost', port=6379)
