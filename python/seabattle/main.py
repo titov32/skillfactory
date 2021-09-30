0f7d55c39e39d36707364e63fdbeff61ca6829ff
Titov Evgeniy <titov32@gmail.com>
2021-09-30 18:03:41 +0700

    added new function
    class App, modern class ship and board

--- python/seabattle/main.py
+++ python/seabattle/main.py
@@ -31,9 +31,17 @@ class Ship:
         self.contur = []
         self.ship_position = []
         self.draw_contur()
+        self.start_condition()
 
     def __str__(self):
         return f'{self.ship_position}'
+
+    def __contains__(self, dot):
+        if dot in self.ship_position:
+            if dot.condition == '■':
+                return True
+        else:
+            return False
 
     def draw_contur(self):
         x, y = self.stern.x, self.stern.y
@@ -46,19 +54,55 @@ class Ship:
             for i in range(x - 1, x + self.size + 1):
                 self.contur.extend([Dot(i, y - 1), Dot(i, y), Dot(i, y + 1)])
 
+    def start_condition(self):
+        for i in self.ship_position:
+            i.condition = '■'
+
+    def is_dead(self):
+        count = 0
+        for dot in self.ship_position:
+            if dot.condition == '■':
+                count += 1
+        if count == 0:
+            return True
+        else:
+            return False
+
+    def shot(self, dot):
+        if dot not in self.ship_position:
+            return 'mimo'
+        else:
+            position = self.ship_position.index(dot)
+            self.ship_position[position].condition = 'X'
+            print(('Попал'))
+        if self.is_dead():
+            print('The ship is dead')
+
 
 class Board:
     def __init__(self, size):
+        self.size = size
         self.fields = [[Dot(x, y) for x in range(size)] for y in range(size)]
         self.placing_fields = []
         self.border = []
+        self.ships = []
         self.fill_border()
+        self.place_ships()
 
     def __contains__(self, item):
         for coord in self.fields:
             if item in coord:
                 return True
         return False
+
+    def place_ships(self):
+        ships_size = [3, 2, 2, 1, 1, 1, 1]
+        for size in ships_size:
+            while True:
+                ship = Ship(Dot(randint(0, size_board), randint(0, size_board)), size, randint(0, 1))
+                if self.append_good(ship):
+                    self.ships.append(ship)
+                    break
 
     def fill_border(self):
         """"
@@ -95,23 +139,67 @@ class Board:
             else:
                 return False
         except IndexError:
-            print('Добавление не возможно')
             return False
+
+    def show_board(self):
+        tmp_list = [str(i) for i in range(self.size - 1)]
+        os_x = '  ' + ' '.join(tmp_list)
+        print(os_x)
+        for count, i in enumerate(board.fields[:-1]):
+            print(count, end=' ')
+            for j in i[:-1]:
+                print(j, end=' ')
+            print()
+
+
+class Player:
+    def __init__(self):
+        self.board = Board(size_board)
+
+    def shot(self, dot):
+        pass
+
+
+class Bot:
+    pass
+
+
+class App:
+    def __init__(self):
+        # self.player1 = Player
+        # self.player2 = Player
+        self.board_human = Board(7)
+        self.board_bot = Board(7)
+
+    def initial(self):
+        print('Приветствуем Вас в игре морской бой')
+        print('Для стрельбы введите координты через пробел')
+
+    def run(self):
+        while True:
+
+            try:
+                x, y = input('введите кординаты через пробел   ').split(' ')
+                x = int(x)
+                y = int(y)
+
+            except ValueError:
+                print('введите 2 корректныx значения')
+                continue
 
 
 if __name__ == '__main__':
     size_board = 7
     board = Board(size_board + 1)
-    ships_size = [3, 2, 2, 1, 1, 1, 1]
-    for size in ships_size:
-        while True:
-            ship = Ship(Dot(randint(0, size_board), randint(0, size_board)), size, randint(0, 1))
-            if board.append_good(ship):
-                print(f'корабль {ship} добавлен')
-                break
 
-    print('0 1 2 3 4 5 6 7')
-    for i in board.fields:
-        for j in i:
-            print(j, end=' ')
-        print()
+    board.show_board()
+
+    a = Dot(1, 1)
+    ship1 = Ship(a, 3, 1)
+    b = Dot(5, 1)
+    ship2 = Ship(b, 2, 1)
+    l = [ship1, ship2]
+
+    game = App()
+    game.initial()
+    #game.run()
