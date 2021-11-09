d766d99322d8ff85a33a54c186bd29136b635fa4
Evgeniy <titov32@gmail.com>
2021-11-10 00:10:53 +0700

    Switch cache with memcached to redis
    edit: setting.py for redis cache
    added cache template

--- python/projectDjango/NewsPaper/NewsPaper/settings.py
+++ python/projectDjango/NewsPaper/NewsPaper/settings.py
@@ -182,11 +182,19 @@ MANAGERS = [
 ]
 
 
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+# }
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
-
 
--- python/projectDjango/NewsPaper/news/urls.py
+++ python/projectDjango/NewsPaper/news/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import PostsList, PostDetail, PostsSearch, PostCreateView, PostsUpdate, PostsDelete  # импортируем наше представление
+from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
-    path('', PostsList.as_view()),
+    path('', cache_page(60)(PostsList.as_view())),
     # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
     path('<int:pk>', PostDetail.as_view(), name='post_detail' ),
     path('search/', PostsSearch.as_view()),
